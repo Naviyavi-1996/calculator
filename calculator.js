be891851384e735b37b0a4cb3fba9128a80f0b81
Naviyavi-1996 <naviyaisahac@gmail.com>
2024-08-29 11:13:13 +0530

    final commit

---
+++ calculator.js
@@ -0,0 +1,24 @@
+//function to display content
+function display(num)
+{
+    result.value+=num
+}
+function clearAll()
+{
+    result.value=""
+}
+function back()
+{
+    result.value=result.value.slice(0,-1)
+}
+function equal()
+{
+    try{
+    result.value=eval(result.value)
+    }
+    catch(error)
+    {
+         result.value="Error"
+         setTimeout(()=>{result.value=""},1000)
+    }
+}
